47a276c44b58a6e01b9840fd16a7fe94c510bf92
StylingOnZ <theanhaptx@gmail.com>
2024-11-07 16:42:18 +0700

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form_Sim
+{
+    public class DatabaseConnection
+    {
+        private readonly string connectionString = "Server=ADMIN-PC\\SQLEXPRESS;Database=SimThe;Integrated Security=True;";
+
+        public DataTable GetSimData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Sim ORDER BY NgayKichHoat ASC";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Sim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSimData();
+
+        }
+        private void LoadSimData()
+        {
+            DatabaseConnection dbConn = new DatabaseConnection();
+            DataTable simData = dbConn.GetSimData();
+            dataGridView1.DataSource = simData;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
